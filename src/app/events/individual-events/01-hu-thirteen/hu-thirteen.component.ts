10a2b2f40e87f62535204838dd575770a878c1c4
Jeremy <Jconn27@gmail.com>
2019-01-25 02:11:43 -0500

    fix for broken builds

--- src/app/events/individual-events/01-hu-thirteen/hu-thirteen.component.ts
+++ src/app/events/individual-events/01-hu-thirteen/hu-thirteen.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hu-thirteen.component.less']
 })
 export class HuThirteenComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
